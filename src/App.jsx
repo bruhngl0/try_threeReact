8a69709d6556bdaaeb7ca9698eb635205b8909c6
bruh.ngl0 <adityashrm500@gmail.com>
2023-05-12 05:59:32 +0530

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import React from "react"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Header from "./components/Header"
+import Coins from "./components/Coins"
+import Exchange from "./components/Exchange"
+import CoinDetails from "./components/CoinDetails"
+import Home from "./components/Home"
 
 
 
@@ -6,7 +13,16 @@ function App() {
   
 
   return (
-    <h1>lol</h1>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element= {<Home />}/>
+        <Route path="/coins" element= {<Coins />}/>
+        <Route path="/exchanges" element= {<Exchange />}/>
+        <Route path="/coin/:id" element= {<CoinDetails />}/>
+        
+      </Routes>
+    </Router>
    
   )
 }
